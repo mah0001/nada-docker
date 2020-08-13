350af27275ddd971ab0a7c6784e1633581d885b4
Mehmood Asghar <mah0001@gmail.com>
2020-08-13 11:12:22 -0400

    minor fixes to use docker build

---
+++ .docker/db_test.php
@@ -0,0 +1,21 @@
+<pre>
+<?php
+$serverName = getenv('DB_HOST'); //serverName\instanceName
+$connectionInfo = array( 
+                "Database"=> getenv('DB_DATABASE'), 
+                "UID"=>getenv('DB_USERNAME'), 
+                "PWD"=>getenv('DB_PASSWORD'));
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+if( $conn ) {
+     echo "Connection established.<br />";
+}else{
+     echo "Connection could not be established.<br />";
+     die( print_r( sqlsrv_errors(), true));
+}
+
+
+
+
+
+
